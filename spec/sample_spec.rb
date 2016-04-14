4ec40e10645af870bb74235de9b9b6275ac20628
meSgolberg <serg_golb@hotmail.com>
2016-04-14 17:28:25 -0400

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,6 +1,6 @@
 for i in 0..500
   describe "These are sample RSpec tests Serg" do
-    it "This is test ##{i}. FAIL" do
+    it "This is test ##{i}. There are test failures" do
     end
   end
 end
